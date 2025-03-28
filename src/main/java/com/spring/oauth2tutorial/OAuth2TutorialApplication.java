821469bbf9c6ff5406883b1e1716528f783fb7cd
kimseonj <kimsunjun1221@gmail.com>
2025-03-28 18:05:28 +0900

    feaf: OAuth2-tutorial 생성

---
+++ src/main/java/com/spring/oauth2tutorial/OAuth2TutorialApplication.java
@@ -0,0 +1,13 @@
+package com.spring.oauth2tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2TutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OAuth2TutorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/oauth2tutorial/OAuth2TutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.oauth2tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2TutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
